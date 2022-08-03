cd7d5c390563a25657251fa1c48d0b412c8604b2
gshrestha <Gunu.Shrestha@ibm.com>
2022-08-02 22:56:14 -0700

    Add /api/send-to-queue function

--- src/main/java/com/ibm/mqclient/service/MQService.java
+++ src/main/java/com/ibm/mqclient/service/MQService.java
@@ -41,11 +41,9 @@ public class MQService {
 			String helloWorld = "Hello World!";
 			jmsTemplate.convertAndSend(queueName, helloWorld);
 			LOG.debug("Successfully Sent message: {} to the queue " + queueName, helloWorld);
-			// LOG.debug("Successfully Sent message: {} to the queue", helloWorld);
 			return helloWorld;
 		} catch (JmsException ex) {
 			throw new AppException("MQAPP001", "Error sending message to the queue " + queueName, ex);
-			// throw new AppException("MQAPP001", "Error sending message to the queue.", ex);
 		}
 	}
 	
